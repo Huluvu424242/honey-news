041346a943fe467c89c8aaa880f9907a85036e9e
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-04-20 21:21:40 +0200

    [feature/#13-router] analyse redirect
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/redirect.js
+++ src/redirect.js
@@ -3,16 +3,20 @@
 // Copyright (c) 2016 Rafael Pedicini, licensed under the MIT License
 
 let redirect404 = () => {
-  var segmentCount = 0;
+  var segmentCount = 1;
   var location = window.location;
+
+  const origin = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
+  const base = location.pathname.split('/').slice(0, 1 + segmentCount).join('/');
+
   location.replace(
-    location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') +
-    location.pathname.split('/').slice(0, 1 + segmentCount).join('/') + '/?p=/' +
+    origin +
+    base + '/?p=/' +
     location.pathname.slice(1).split('/').slice(segmentCount).join('/').replace(/&/g, '~and~') +
     (location.search ? '&q=' + location.search.slice(1).replace(/&/g, '~and~') : '') +
     location.hash
   );
-}
+};
 
 let recieveRedirect = () => {
   (function(location) {
@@ -31,4 +35,4 @@ let recieveRedirect = () => {
       }
     }
   }(window.location));
-}
+};

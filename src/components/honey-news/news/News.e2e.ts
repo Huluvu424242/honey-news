103ea897790b3d3ab94f71afe7e3e2c897be6451
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-09-26 20:12:05 +0200

    [feature/bootstrap] refactoring
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/components/honey-news/news/News.e2e.ts
+++ src/components/honey-news/news/News.e2e.ts
@@ -1,16 +1,16 @@
 import {newE2EPage} from '@stencil/core/testing';
-import {E2EElement, E2EPage} from "@stencil/core/testing/puppeteer/puppeteer-declarations";
+import {E2EPage} from "@stencil/core/testing/puppeteer/puppeteer-declarations";
 
 describe('E2E: honey-news satisfy', () => {
 
   describe('simple check for present at page', () => {
 
     let page: E2EPage;
-    let element: E2EElement;
+    // let element: E2EElement;
 
     beforeEach(async () => {
       page = await newE2EPage({html: `<honey-news></honey-news>`});
-      element = await page.find('honey-news');
+      // element = await page.find('honey-news');
     });
 
     it('Add Feed Button is present', async () => {

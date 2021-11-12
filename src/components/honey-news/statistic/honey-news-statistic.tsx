4a977f4a9430c6b04e4af1831b7c7fd5e9c892d1
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-11-12 18:42:53 +0100

    [feature/styling] refactoring
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/components/honey-news/statistic/honey-news-statistic.tsx
+++ src/components/honey-news/statistic/honey-news-statistic.tsx
@@ -1,4 +1,4 @@
-import {Component, Element, h, Host, Prop, State} from "@stencil/core";
+import {Component, h, Host, State} from "@stencil/core";
 import {Subscription} from "rxjs";
 import {StatisticData} from "@huluvu424242/liona-feeds/dist/esm/feeds/statistic";
 import {StatisticLoader} from "./StatisticLoader";
@@ -10,43 +10,21 @@ import {StatisticLoader} from "./StatisticLoader";
 })
 export class HoneyNewsStatistic {
 
-  /**
-   * Host Element
-   */
-  @Element() hostElement: HTMLElement;
-
-
-  /**
-   * Id des Host Elements, falls nicht verfügbar wird diese generiert
-   */
-  ident: string;
-
-
-  statisticLoader: StatisticLoader = new StatisticLoader();
-
+  protected static statisticLoader: StatisticLoader = new StatisticLoader();
 
   @State() statistic: StatisticData[];
   statisticSubscription: Subscription;
 
-
-  /**
-   * enable console logging
-   */
-  @Prop() verbose: boolean = false;
-
   public connectedCallback() {
-    // States initialisieren
-    this.ident = this.hostElement.id ? this.hostElement.id : Math.random().toString(36).substring(7);
     this.statisticSubscription = this.subscribeStatistics();
   }
-
 
   public disconnectedCallback() {
     this.statisticSubscription.unsubscribe();
   }
 
   protected subscribeStatistics(): Subscription {
-    return this.statisticLoader.subscribeStatistiken()
+    return HoneyNewsStatistic.statisticLoader.subscribeStatistiken()
       .subscribe((statisticDatas: StatisticData[]) => this.statistic = [...statisticDatas]);
   }
 

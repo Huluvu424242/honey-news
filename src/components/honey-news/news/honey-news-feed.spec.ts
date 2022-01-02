310f5626f7b31cd4e0e519871a80db1e0df2c327
huluvu424242 <huluvu424242@gmail.com>
2022-01-02 18:51:19 +0100

    Refactoring
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- src/components/honey-news/news/honey-news-feed.spec.ts
+++ src/components/honey-news/news/honey-news-feed.spec.ts
@@ -7,6 +7,6 @@ test.describe("Prüfe Startseite", () => {
 
   test("Zeige Disclaimer an.", async ({  page }) => {
     const locator = page.locator("honey-disclaimer");
-    await expect(locator).toContainText("!!! Das 1ist eine Demo Seite welche alle Feature der App zeigen soll - aus diesem Grund ist auch die Statistik eingeschaltet !!!Es werden nur Daten zu den abgerufenen Feeds gespeichert (in memory) wie: URL, Anzahl der Abfragen und Anzahl valider Anworten.Sollten Sie die Speicherung nicht wünschen - dann geben Sie bitte keinen neuen Feed ein.Vielen Dank für Ihr Verständnis.");
+    await expect(locator).toContainText("!!! Das ist eine Demo Seite welche alle Feature der App zeigen soll - aus diesem Grund ist auch die Statistik eingeschaltet !!!Es werden nur Daten zu den abgerufenen Feeds gespeichert (in memory) wie: URL, Anzahl der Abfragen und Anzahl valider Anworten.Sollten Sie die Speicherung nicht wünschen - dann geben Sie bitte keinen neuen Feed ein.Vielen Dank für Ihr Verständnis.");
   });
 });

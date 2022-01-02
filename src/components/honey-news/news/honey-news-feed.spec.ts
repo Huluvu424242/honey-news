2c1848004b819f278881305675195c4a19a9ee73
Huluvu424242 <huluvu424242@gmail.com>
2022-01-02 23:14:21 +0100

    test fixed
    Signed-off-by: Huluvu424242 <huluvu424242@gmail.com>

--- src/components/honey-news/news/honey-news-feed.spec.ts
+++ src/components/honey-news/news/honey-news-feed.spec.ts
@@ -14,4 +14,11 @@ test.describe("Prüfe Startseite", () => {
     const locator = page.locator("honey-disclaimer");
     await expect(locator).toContainText("!!! Das ist eine Demo Seite welche alle Feature der App zeigen soll - aus diesem Grund ist auch die Statistik eingeschaltet !!!Es werden nur Daten zu den abgerufenen Feeds gespeichert (in memory) wie: URL, Anzahl der Abfragen und Anzahl valider Anworten.Sollten Sie die Speicherung nicht wünschen - dann geben Sie bitte keinen neuen Feed ein.Vielen Dank für Ihr Verständnis.");
   });
+
+  test("Disclaimer verschwindet nach dem Schließen.", async ({  page }) => {
+    const disclaimerClose = await page.locator("div.message-header button.delete");
+    await disclaimerClose.waitFor({state: 'visible'});
+    await disclaimerClose.click();
+    await disclaimerClose.waitFor({state: 'detached'});
+  });
 });

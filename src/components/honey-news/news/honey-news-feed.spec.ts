37a0539f5ce473ab68f0be3f3735830f09d230c2
Huluvu424242 <huluvu424242@gmail.com>
2022-01-02 21:27:08 +0100

    bildvergleich aufgenommen
    Signed-off-by: Huluvu424242 <huluvu424242@gmail.com>

--- src/components/honey-news/news/honey-news-feed.spec.ts
+++ src/components/honey-news/news/honey-news-feed.spec.ts
@@ -5,7 +5,12 @@ test.describe("Prüfe Startseite", () => {
     await page.goto(baseURL);
   });
 
-  test("Zeige Disclaimer an.", async ({  page }) => {
+  test('Aussehen ist gleich geblieben', async ({baseURL,page}) => {
+    await page.goto(baseURL);
+    expect(await page.screenshot()).toMatchSnapshot('./test/resources/startseite-bulma.png');
+  });
+
+  test("Disclaimer wird angezeigt.", async ({  page }) => {
     const locator = page.locator("honey-disclaimer");
     await expect(locator).toContainText("!!! Das ist eine Demo Seite welche alle Feature der App zeigen soll - aus diesem Grund ist auch die Statistik eingeschaltet !!!Es werden nur Daten zu den abgerufenen Feeds gespeichert (in memory) wie: URL, Anzahl der Abfragen und Anzahl valider Anworten.Sollten Sie die Speicherung nicht wünschen - dann geben Sie bitte keinen neuen Feed ein.Vielen Dank für Ihr Verständnis.");
   });

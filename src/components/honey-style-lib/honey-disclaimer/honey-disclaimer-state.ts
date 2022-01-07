d2bcfa992ecfd52c63b0f41cdeaee9d3dbd38c50
Huluvu424242 <huluvu424242@gmail.com>
2022-01-07 20:40:50 +0100

    docu updated
    Signed-off-by: Huluvu424242 <huluvu424242@gmail.com>

--- src/components/honey-style-lib/honey-disclaimer/honey-disclaimer-state.ts
+++ src/components/honey-style-lib/honey-disclaimer/honey-disclaimer-state.ts
@@ -1,4 +1,13 @@
-
+/**
+ * Eigentlich Teil der Komponente aber ausgelagert weil:
+ *
+ * To allow efficient bundling, modules using @Component() can only have a single export which is the
+ * component class itself. Any other exports should be moved to a separate file. For further
+ * information check out: https://stenciljs.com/docs/module-bundling
+ *
+ * Singleton to hold the Components State
+ *
+*/
 class HoneyDisclaimerState {
   public wasRead: boolean = false;
 

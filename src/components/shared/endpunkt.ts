27981f270dea5453b6b0daa58b34404b36573c09
huluvu424242 <huluvu424242@gmail.com>
2022-01-29 00:19:34 +0100

    endpunkt definiert
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

---
+++ src/components/shared/endpunkt.ts
@@ -0,0 +1,42 @@
+export interface EndpunktBase {
+  readonly host: string;
+  readonly port: number;
+
+}
+
+export interface EndpunktResource {
+  readonly path: string;
+  readonly query: string;
+}
+
+export class Endpunkt implements EndpunktBase, EndpunktResource {
+  readonly host: string;
+  readonly port: number;
+  readonly path: string;
+  readonly query: string;
+
+  constructor(host: string, port: number, path: string, query: string) {
+    this.host = host;
+    this.port = port;
+    this.path = path;
+    this.query = query;
+  }
+
+  public getEndpunktBase(): EndpunktBase {
+    return this;
+  }
+
+  public getEndpunktResource(): EndpunktResource {
+    return this;
+  }
+
+  public replaceEndpunktBaseIfGiven(host: string, port: number): Endpunkt {
+    if (host && port) {
+      return new Endpunkt(host, port, this.path, this.query);
+    } else {
+      return new Endpunkt(this.host, this.port, this.path, this.query);
+    }
+  }
+
+}
+
